fabdc3a9f6938ffe1c3f233b8796f94e9298a011
ranto <ranto.rnv@gmail.com>
2025-10-20 17:23:30 +0300

    deps fix

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -12,7 +12,5 @@ module.exports = {
         allowUndefined: true,
       },
     ],
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
-    ['@babel/plugin-proposal-class-properties', { loose: true }]
   ],
 };
