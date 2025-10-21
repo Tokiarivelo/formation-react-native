cfeb72ce8e290fba7a6693d0207354c1eb0c45ba
ranto <ranto.rnv@gmail.com>
2025-10-21 09:50:31 +0300

    user api call + react query

--- frontend/src/modules/user/api.ts
+++ frontend/src/modules/user/api.ts
@@ -2,13 +2,39 @@ import { AxiosError } from "axios";
 import { apiClient } from "../../services/axiosInstance";
 import { User } from "../../types/api";
 
-export const getUserMe = async (): Promise<User> => {
-    try {
-        const response = await apiClient.get('/users/me');
-        return response.data;
+export const usersApi = {
+    async getAll(): Promise<User[]> {
+        try {
+            const response = await apiClient.get<User[]>('/users');
+            return response.data;
+        }
+        catch (error) {
+            const axiosError = error as AxiosError;
+            throw axiosError.response?.data || { message: 'Fetch users failed.' };
+        }
+    },
+
+    async getMe(): Promise<User> {
+        try {
+            const response = await apiClient.get<User>('/users/me');
+            return response.data;
+        }
+        catch (error) {
+            const axiosError = error as AxiosError;
+            throw axiosError.response?.data || { message: 'Fetch users failed.' };
+        }
+    },
+
+    async getId(id: string): Promise<User> {
+        try {
+            const response = await apiClient.get<User>(`/users/${id}`);
+            return response.data;
+        } catch (error) {
+            const axiosError = error as AxiosError;
+            throw axiosError.response?.data || { message: 'Fetch users failed.' };
+        }
     }
-    catch (error) {
-        const axiosError = error as AxiosError;
-        throw axiosError.response?.data || { message: 'Fetch user failed.' };
-    }
+
+    //async updateMe(): 
+
 }
--- frontend/src/modules/user/hooks/useUsers.ts
+++ frontend/src/modules/user/hooks/useUsers.ts
@@ -1,10 +1,13 @@
 import { useQuery } from "@tanstack/react-query"
-import { getUserMe } from "../api"
+import { usersApi } from "../api"
+import { useAuthStore } from "../../../store/authStore";
 
 export const useAuthUser = () => {
+    const isLoggedIn = useAuthStore((s) => s.isLoggedIn);
     return useQuery({
-        queryFn: getUserMe,
+        queryFn: () => usersApi.getMe(),
         queryKey: ['auth-user'],
+        enabled: !!isLoggedIn,
         staleTime: 5 * 60 * 1000, // 5 minutes
     })
 }
